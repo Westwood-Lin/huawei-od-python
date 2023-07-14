0070f390e38f662b5b2798cb2c23d936b93ef2fd
Yalin Feng <1070298513@qq.com>
2023-07-14 16:17:13 +0800

    141 代码和文档

--- codes/others100/141_Number_of_squares.py
+++ codes/others100/141_Number_of_squares.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 from typing import List
 
+
 def is_square(point1: List[int], point2: List[int], point3: List[int], point4: List[int]) -> bool:
     """
      :param point1: 第一个点的坐标，需要是列表类型，列表长度需要为2；其他三个参数以此类推
@@ -26,18 +27,6 @@ def is_square(point1: List[int], point2: List[int], point3: List[int], point4: L
             distances[2] == distances[3]) and (distances[4] == distances[5])
 
 
-def solve_method(n: int, points: List[List[int]]) -> int:
-    count = 0
-    # 通过四重循环来不重复地挑选 4个点，通过调用 is_square 判断是否构成正方形
-    for i in range(n - 3):
-        for j in range(i + 1, n - 2):
-            for k in range(j + 1, n - 1):
-                for p in range(k + 1, n):
-                    if is_square(points[i], points[j], points[k], points[p]):
-                        count += 1
-    return count
-
-
 if __name__ == '__main__':
     # 1. 处理输入
     n = int(input())
@@ -49,43 +38,14 @@ if __name__ == '__main__':
         x, y = map(int, input().split())
         points.append([x, y])
 
-    # 2.调用 solve_method 方法，计算能构成多少个正方形
-    res = solve_method(n, points)
+    # 2. 通过四重循环来不重复地挑选 4个点，通过调用 is_square 判断是否构成正方形
+    count = 0
+    for i in range(n - 3):
+        for j in range(i + 1, n - 2):
+            for k in range(j + 1, n - 1):
+                for p in range(k + 1, n):
+                    if is_square(points[i], points[j], points[k], points[p]):
+                        count += 1
 
-    # 3.打印输出
-    print(res)
-
-
-
-
-"""
-以下被注释掉的部分是4个给定用例的测试。
-注释掉上面的__main__方法，恢复下面的测试代码，可以直接运行main测试方法正确性。
-"""
-
-# import unittest
-#
-# if __name__ == '__main__':
-#     # unittest.main()语句用于执行4个给定的测试用例; 注释掉这条语句才能读取键盘输入
-#     unittest.main()
-#
-# class MethodTest(unittest.TestCase):
-#     def test1(self):
-#         n = 3
-#         points = [[1, 3], [2, 4], [3, 1]]
-#         self.assertEqual(solve_method(n, points), 0)
-#
-#     def test2(self):
-#         n = 4
-#         points = [[0, 0], [1, 2], [3, 1], [2, -1]]
-#         self.assertEqual(solve_method(n, points), 1)
-#
-#     def test3(self):
-#         n = 5
-#         points = [[0, 0], [1, 2], [3, 1], [2, -1], [1, 1]]
-#         self.assertEqual(solve_method(n, points), 1)
-#
-#     def test4(self):
-#         n = 7
-#         points = [[0, 0], [1, 2], [3, 1], [2, -1], [1, 1], [1, 0], [0, 1]]
-#         self.assertEqual(solve_method(n, points), 2)
+    # 3. 打印输出
+    print(count)
